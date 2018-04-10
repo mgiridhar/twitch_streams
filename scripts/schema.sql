3ba132d6d27f7d513badfe1341338aa6d896978c
mgiridhar <mgiridhar11@gmail.com>
2018-04-09 18:58:54 -0700

    adding mysql schema for users

---
+++ scripts/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
